6ea15cbf046f67ea060fd86c790168da2ad6dbed
David Cano <153380900+davichus1303@users.noreply.github.com>
2026-09-17 14:11:35 -0600

    feat: add delete company endpoint

--- src/main/java/com/contatodo/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/contatodo/infrastructure/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/companies").authenticated()
                         .requestMatchers(HttpMethod.POST, "/companies").authenticated()
                         .requestMatchers(HttpMethod.PUT, "/companies/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/companies/**").authenticated()
                         .requestMatchers(HttpMethod.GET, "/expenses").authenticated()
                         .requestMatchers(HttpMethod.POST, "/expenses").authenticated()
                         .requestMatchers("/roles/**").authenticated()
